b10403604dc8844f46b6098952be4662f3556aaf
Thomas Berteau <berteau.thomas@hotmail.fr>
2024-01-11 09:07:03 +0100

    Address controller

---
+++ banking/src/main/java/com/tbert31/banking/controllers/AddressController.java
@@ -0,0 +1,12 @@
+package com.tbert31.banking.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class AddressController {
+
+}
