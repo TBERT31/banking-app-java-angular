44e59866b6ec5fa95ead53d8047b550ffa723faa
Thomas Berteau <berteau.thomas@hotmail.fr>
2024-01-11 09:16:01 +0100

    update

--- banking/src/main/java/com/tbert31/banking/controllers/AddressController.java
+++ banking/src/main/java/com/tbert31/banking/controllers/AddressController.java
@@ -1,12 +1,43 @@
 package com.tbert31.banking.controllers;
 
+import com.tbert31.banking.dto.AddressDto;
+import com.tbert31.banking.services.AddressService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/addresses")
 @RequiredArgsConstructor
 public class AddressController {
+    private final AddressService service;
 
+    @PostMapping("")
+    public ResponseEntity<Integer> save(
+            @RequestBody AddressDto addressDto
+    ){
+        return ResponseEntity.ok(service.save(addressDto));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<AddressDto>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{address-id}")
+    public ResponseEntity<AddressDto> findById(
+            @PathVariable("user-id") Integer addressId
+    ){
+        return ResponseEntity.ok(service.findById(addressId));
+    }
+
+    @DeleteMapping("/{address-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("address-id") Integer addressId
+    ){
+        service.delete(addressId);
+        return ResponseEntity.accepted().build();
+    }
 }
